361785666c9e12ebeea880270dc5bb2e0798aace
Gustavo Reis <gusbemacbe@gmail.com>
2020-08-10 23:34:10 -0300

    Updates

---
+++ component---src-pages-page-2-pt-js-994ed3574c88e2cd4746.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{"/6iV":function(a,e,n){"use strict";n.r(e);var t=n("q1tI"),i=n.n(t),l=n("+ZDr"),o=n.n(l),c=n("C6dP");e.default=function(a){return i.a.createElement(c.a,{location:a.location},i.a.createElement("div",null,i.a.createElement("h1",null,"Oi da segunda pagina!"),i.a.createElement("p",null,"Bem viando a pagina 2"),i.a.createElement(o.a,{to:"/pt/"},"Voltar pro início")))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-pt-js-994ed3574c88e2cd4746.js.map
--- styles-c886cc29310f10f235a2.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[13],[]]);
-//# sourceMappingURL=styles-c886cc29310f10f235a2.js.map
